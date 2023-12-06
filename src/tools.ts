6f3ed09e22e23b3b44fc2d8e34002916f622a483
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-06 07:19:06 +0100

    added test coverage

--- src/tools.ts
+++ src/tools.ts
@@ -8,3 +8,14 @@
 export const capitalizeFirstLetter = (text: string) => {
 	return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 };
+
+/**
+ * sentencize()
+ * 
+ * sentencize('this is a message')
+ * 
+ * "This is a message."
+ */
+export const sentencize = (text: string) => {
+	return "nnn";
+};
